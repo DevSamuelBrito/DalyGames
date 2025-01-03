a57b7167441943892f2a5e4257b764f47d0c2eb5
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-03 14:41:22 -0300

    criando a parte de API e puxando as images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
-export default function Home() {
-  return <main className="flex">
-    <h1>Hello World!</h1>
-  </main>;
+import { Container } from "@/components/Container";
+
+import Link from "next/link";
+import Image from "next/image";
+import { GamesProps } from "@/utils/types/game";
+
+async function getDalyGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URl}/next-api/?api=game_day`
+    );
+    return res.json();
+  } catch (error) {
+    throw new Error("Erro ao buscar os dados");
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GamesProps = await getDalyGame();
+
+  return (
+    <main className="flex">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um Jogo Exclusivo para Você
+        </h1>
+
+        <Link href={`/games/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dalyGame.image_url}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+              alt={dalyGame.title}
+            ></Image>
+          </section>
+        </Link>
+      </Container>
+    </main>
+  );
 }
